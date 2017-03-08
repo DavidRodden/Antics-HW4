6b7c6ad8de5731ac6f709d9212c25c40c1a0ad64
David Rodden <roddend17@up.edu>
2017-03-07 17:49:37 -0800

    initialization of population index

--- AI/GeneticAI.py
+++ AI/GeneticAI.py
@@ -43,6 +43,8 @@ class AIPlayer(Player):
     # then resets the fitness list to default values
     #
     def initializeGenePopulation(self, popSize):
+        # index set to 0 for the initialization of a population
+        self.currentPopFitness = 0
         # gene includes tuples of board position, random large number
         gene = []
         for g in range(0, popSize):
